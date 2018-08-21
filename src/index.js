d0fbd9ced8d3801a98621dce48854a512fed4614
d-chaps <42569630+d-chaps@users.noreply.github.com>
2018-08-21 13:47:57 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import MainApp from './MainApp';
 import registerServiceWorker from './registerServiceWorker';
 

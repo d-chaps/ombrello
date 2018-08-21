1b9d786a8a31f244cffeefb9817016ad36d66ef8
d-chaps <42569630+d-chaps@users.noreply.github.com>
2018-08-21 13:39:03 +0200

    Add files via upload

---
+++ src/WeatherColumn.js
@@ -0,0 +1,59 @@
+import React, { Component } from 'react';
+import './style/App.css';
+import raning from './style/weatherIcons/raning.png';
+import clear from './style/weatherIcons/clear.png';
+import cloudy from './style/weatherIcons/fewclouds.png';
+import thunderstorm from './style/weatherIcons/thunderstorm.png';
+
+class WeatherColumn extends Component {
+
+    render() {
+
+        let isRaining = false;
+        let isClear = false;
+        let isCloudy = false;
+        let isThunderstorm = false;
+
+        if (this.props.weatherDescription === 'broken clouds' || this.props.weatherDescription === 'light rain') {
+            isRaining = true
+        } else if (this.props.weatherDescription === 'clear sky') {
+            isClear = true;
+        } else if (this.props.weatherDescription === 'few clouds' || this.props.weatherDescription === 'overcast clouds'
+            || this.props.weatherDescription === 'scattered clouds') {
+            isCloudy = true;
+        }
+
+        return(
+        <div>
+            <div className="dateNtime">
+                {(this.props.dateNtime.slice(0,16))}</div>
+            { isRaining &&
+            <div className="imgBox" alt="rain"><img src={raning}/></div>
+            }
+            { isClear &&
+            <div className="imgBox" alt="clear"><img src={clear} /></div>
+            }
+            { isCloudy &&
+            <div className="imgBox" alt="cloudy"><img src={cloudy}/></div>
+            }
+
+            <div className="weatherDescription">
+                {this.props.weatherDescription}
+            </div>
+            <div className="moreInfo">
+                <div className="bold">Temperature:</div> {(this.props.weatherTemp - 273.15).toFixed(1)}°C
+            </div>
+            <div className="moreInfo">
+                <div className="bold">Pressure:</div> {this.props.pressure} hPa
+            </div>
+            <div className="moreInfo">
+                <div className="bold">Humidity:</div>    {this.props.humidity}%
+            </div>
+            <div className="moreInfo">
+                <div className="bold">Wind Speed:</div> {this.props.windSpeed} km/h
+            </div>
+        </div>
+        )
+    }
+}
+export default WeatherColumn;
